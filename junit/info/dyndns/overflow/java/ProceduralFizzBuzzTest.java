8eee6c316aa10241efabb8c7e7f243c379323302
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2013-10-16 17:06:13 +0900

    Add sampleTest

---
+++ junit/info/dyndns/overflow/java/ProceduralFizzBuzzTest.java
@@ -0,0 +1,14 @@
+package info.dyndns.overflow.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProceduralFizzBuzzTest {
+
+	@Test
+	public void testResult() {
+		fail("‚Ü‚¾ŽÀ‘•‚³‚ê‚Ä‚¢‚Ü‚¹‚ñ");
+	}
+
+}
