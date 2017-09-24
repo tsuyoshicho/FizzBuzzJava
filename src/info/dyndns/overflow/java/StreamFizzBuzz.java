4965bd42a303446f921daac8f9f969cf7a9f391c
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2017-09-24 20:28:29 +0900

    fix bug5

--- src/info/dyndns/overflow/java/StreamFizzBuzz.java
+++ src/info/dyndns/overflow/java/StreamFizzBuzz.java
@@ -17,7 +17,7 @@ public class StreamFizzBuzz extends AbstractFizzBuzz {
 	@Override
 	Map<Integer, String> createResult(Map<Integer, String> map) {
 		 Map<Integer, String> data = IntStream.range(0,this.max)
-		 																			.forEach(i -> map.put(i,FizzBuzzFactoryFizzBuzzCalc(i)));
+		 																			.forEach(i -> map.put(i,FizzBuzzFactory.FizzBuzzCalc(i)));
 			return map;
 	}
 }
